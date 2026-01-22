9e0c59be95bdab00ca9235dd2846c4f169e16fd0
timonrieger <mail@timonrieger.de>
2026-01-22 16:27:00 +0100

    type fix

--- immich/cli/types.py
+++ immich/cli/types.py
@@ -2,7 +2,7 @@ from typing import Literal, Optional, Union
 
 from pydantic import BaseModel
 
-MaybeBaseModel = Optional[Union[BaseModel, list[BaseModel]]]
+MaybeBaseModel = Optional[Union[BaseModel, list[BaseModel], str]]
 FormatMode = Literal["pretty", "json", "table"]
 PrintType = Literal["info", "warning", "error", "debug", "success", "json", "text"]
 
